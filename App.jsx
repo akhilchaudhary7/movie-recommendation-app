abdf56480114d666fb1a3dafcfd21e71f89e3ef7
akhilchaudhary7 <akhilchaudhary5112@gmail.com>
2025-08-22 00:24:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import Dashboard from "./pages/Dashboard";
+import SearchResults from "./components/Dashboard/SearchResults";
+import ListPage from "./components/Dashboard/ListPage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import FilteredMovies from "./pages/FilteredMovies";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/filtered-movies" element={<FilteredMovies />} />
+          <Route path="/search" element={<SearchResults/>} />
+          <Route path="/lists/:id" element={<ListPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
